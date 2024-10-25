4fdad9a0a1960e3173a1b5f833014114fc266a5f
Daniel Rodriguez Alonso <danielrodriguezalonso02@gmail.com>
2024-10-25 16:59:16 +0100

    Create show details function

--- run.py
+++ run.py
@@ -2,10 +2,29 @@
 
 import search
 
-ab = search.GPSProblem('A', 'B'
-                       , search.romania)
+roads = [
+    ['A', 'B'], 
+    ['O', 'E'], 
+    ['G', 'Z'], 
+    ['N', 'D'], 
+    ['M', 'F']
+    ]
 
-print(search.breadth_first_graph_search(ab).path())
-print(search.depth_first_graph_search(ab).path())
-print(search.branch_and_bound_search(ab).path())
-print(search.branch_and_bound_search_underestimation(ab).path())
+
+def show_details(roads):
+    """Displays a well-structured message for each search"""
+    for inital, goal in roads:
+        problem = search.GPSProblem(inital, goal, search.romania)
+        print(f"\n-------------------- <Node:{problem.initial}> -> <Node:{problem.goal}> ----------------------")
+        print("---- Busquedad en anchura ----")
+        print(search.breadth_first_graph_search(problem).path())
+        print("\n---- Busquedad en profundidad ----")
+        print(search.depth_first_graph_search(problem).path())
+        print("\n---- Busquedad por ramificación y acotación ----")
+        print(search.branch_and_bound_search(problem).path())
+        print("\n---- Busquedad por ramificación y acotación con subestimación ----")
+        print(search.branch_and_bound_search_underestimation(problem).path())
+        print(f"---------------------------------------------------------------------------------------------")
+
+
+show_details(roads)
